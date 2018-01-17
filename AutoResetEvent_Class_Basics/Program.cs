e6104d91059e2f4350780ace8f61a5f6f38ba0ab
Harsh via Mac <Harsh007kumar@gmail.com>
2018-01-18 03:09:34 +0530

    AutoResetEvent_Class_Basics

---
+++ AutoResetEvent_Class_Basics/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+
+namespace AutoResetEvent_Class_Basics
+{
+    class MainClass
+    {
+        static AutoResetEvent AutoReset1 = new AutoResetEvent(true);
+        static AutoResetEvent AutoReset2 = new AutoResetEvent(false);
+
+        public static void Main(string[] args)
+        {
+            for (int x = 1; x < 6; x++)
+            {
+                Thread t = new Thread(new ThreadStart(letsPlayWithThreads));
+                t.Name = "Thread" + x;
+                t.Start();
+            }
+            for (int y = 1; y < 5; y++)
+            {
+                Console.WriteLine("Press any key to release an thread from AutoResetEvent 1");
+                Console.ReadLine();
+                AutoReset1.Set();
+                Thread.Sleep(250);
+            }
+            for (int z = 1; z < 6; z++)
+            {
+                Console.WriteLine("Press any key to release an thread from AutoResetEvent 2");
+                Console.ReadLine();
+                AutoReset2.Set();
+            }
+        }
+
+        private static void letsPlayWithThreads()
+        {
+            string name = Thread.CurrentThread.Name;
+            Console.WriteLine("{0} Waiting for AutoResetEvent 1",name);
+            AutoReset1.WaitOne();
+            Console.WriteLine("{0} Releases from AutoResetEvent 1",name);
+
+            Console.WriteLine("{0} Waiting for AutoResetEvent 2",name);
+            AutoReset2.WaitOne();
+            Console.WriteLine("{0} Releases from AutoResetEvent 2",name);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-Program to illustrate basic concepts of MULTITHREADING like :
-1) Lock 2) Monitor  3) Synchronization 4) Mutex  5) Semaphore
-# For more visit - 
-# http://www.c-sharpcorner.com/UploadFile/84c85b/multithreading-with-net/
-# http://www.c-sharpcorner.com/UploadFile/rohatash/multithreading-in-C-Sharp/
+# Program to illustrate basic concepts of MULTITHREADING like :
+# 1) Lock 2) Monitor  3) Synchronization 4) Mutex  5) Semaphore
+For more visit -
+http://www.c-sharpcorner.com/UploadFile/84c85b/multithreading-with-net/
+https://msdn.microsoft.com/en-us/library/system.threading.autoresetevent(v=vs.110).aspx
